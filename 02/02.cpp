09e96c98567ff415db2b67e8a1f16a4420429c91
Simcaa007 <havelkovas.07@spst.eu>
2026-02-17 09:04:42 +0100

    hotovo

--- 02/02.cpp
+++ 02/02.cpp
@@ -5,12 +5,16 @@
 
 using namespace std;
 
+int mocnina(int a, int n);
+int soucet(int n);
+
+
 int main()
 {
     int volba;
-    cout << "Zadejte volbu programu 1(mocnina), 2(ciferny soucet),3(konec)";
 
     do {
+        cout << "Zadejte volbu programu 1(mocnina), 2(ciferny soucet),3(konec)";
         cin >> volba;
 
         switch (volba)
@@ -19,25 +23,16 @@ int main()
             int a, n;
             cout << "zadejte cele cislo a: ";
             cin >> a;
-            cout << "zadejte cele nezaporne cislo n: ";
+            cout << "zadejte cele nezaporne cislo n: "; 
             cin >> n;
-            int mocnina = 1;
-            for (int i = 0; i < n; i++) {
-                mocnina *= a;
-            }
-            cout << "mocnina je: " << mocnina << endl;
+            cout << mocnina(a, n) << endl;
 
         }break;
         case 2: {
             int n;
             cout << "Zadejte cele nezaporne cislo n: ";
             cin >> n;
-            int soucet = 0;
-            while (n > 0) {
-                soucet += n % 10;
-                n = n / 10;
-            }
-            cout << "Ciferny soucet cisla je: " << soucet << endl;
+            cout << "Ciferny soucet cisla je: " << soucet(n) << endl;
         }break;
 
         case 3: {
@@ -50,4 +45,22 @@ int main()
         }
 
     } while (volba != 3);
+}
+
+int mocnina(int a, int n) {
+    int mocnina = 1;
+    for (int i = 0; i < n; i++) {
+        mocnina *= a;
+    }
+
+    return mocnina;
+}
+
+int soucet(int n) {
+    int soucet = 0;
+    while (n > 0) {
+        soucet += n % 10;
+        n = n / 10;
+    }
+    return soucet;
 }
---
+++ 03/03.cpp
@@ -0,0 +1,11 @@
+﻿// 03.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
