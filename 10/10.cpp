b62d558decb71bc74171f1102c397ccc78f40355
Simcaa007 <havelkova.si@seznam.cz>
2026-02-21 15:48:46 +0100

    hotovo

---
+++ 10/10.cpp
@@ -0,0 +1,41 @@
+﻿// 10.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string dokonalaCisla(int a, int b);
+
+int main()
+{
+    int a, b;
+
+    cout << "Zadej cele kladne cislo a: "; cin >> a;
+    cout << "Zadej cele kladne cislo b: "; cin >> b;
+
+	cout << "Dokonala cisla z rozmezi jsou: " << dokonalaCisla(a, b) << endl;
+	return 0;
+}
+
+
+string dokonalaCisla(int a, int b) {
+	
+    string vysledek = "";
+
+	for (int i = a; i <= b; i++) {
+		int cislo = i;
+		int soucet = 0;
+		for (int j = 1; j < cislo; j++) {
+			if (cislo % j == 0) {
+				soucet += j;
+			}
+		}
+
+		if (soucet == cislo && cislo != 0) {
+			vysledek += to_string(cislo) + "; ";
+		}
+	}
+	return vysledek;
+}
